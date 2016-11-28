8f0ee8a99a30da733f485ec5224fa379a61fc027
Guilherme Victal <guilhermevictal em gmail ponto com>
2016-11-28 20:19:09 -0200

    Add separate cli script

---
+++ sesame/cli.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from argparse import ArgumentParser
+from sg_open import sg_open
+from sg_close import sg_close
+
+def create_parser():
+      parser = ArgumentParser()
+      subparsers = parser.add_subparsers(dest='command')
+      p_open = subparsers.add_parser('open', help='Add rules to open SSH to the local IP')
+      open_group = p_open.add_mutually_exclusive_group(required=True)
+      open_group.add_argument('--name', dest='name', help='Name of a Securiy Group to which to add the SSH rule')
+      open_group.add_argument('-a', '--all', action='store_true', help='If set, will add the SSH rule to all existing Security Groups')
+
+      p_close = subparsers.add_parser('close', help='Removs rules allowing SSH access to the local IP')
+      p_close.add_argument('--name', dest='name', help='Name of a Securiy Group from which to remove the SSH rules')
+      p_close.add_argument('-a', '--all', action='store_true', help='If set, will try to remove all SSH rules from a given SG, or all SSH rules specific to the local IP, otherwise')
+
+      subparsers.add_parser('list-open', help='Lists Security Groups that have SSH rules allowing access from this IP')
+      subparsers.add_parser('list-closed', help='Lists Security Groups that do not have SSH rules allowing access from this IP')
+
+      return parser
+
+
+def main():
+      parser = create_parser()
+      args = parser.parse_args()
+      if args.command == 'close':
+            sg_close(sg_name=args.name, all=args.all)
+      elif args.command == 'open':
+            sg_open(sg_name=args.name, all=args.all)
+
+
+if __name__ == "__main__":
+      main()
---
+++ sesame/sg_open.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from utils import get_external_ip, get_sg_id_from_name
+import boto3
+
+def sg_open(sg_name=None, all=False):
+      sg_id = get_sg_id_from_name(sg_name)
+      ec2 = boto3.resource('ec2')
+      current_ip = get_external_ip()
+      current_cidr = current_ip + "/32"
+      security_group = ec2.SecurityGroup(sg_id)
+      security_group.authorize_ingress(
+                  IpProtocol='tcp',
+                  FromPort=22,
+                  ToPort=22,
+                  CidrIp=current_cidr
+                  )
+
+if __name__ == '__main__':
+      import sys
+      sg_name = sys.argv[1]
+      sg_open(sg_name=sg_name)
