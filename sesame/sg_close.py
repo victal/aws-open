7e248c70e921eb0101a23fdf7d887b3e3dae1b05
Guilherme Victal <guilhermevictal em gmail ponto com>
2017-01-16 22:03:01 -0200

    Implement close all

--- sesame/sg_close.py
+++ sesame/sg_close.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from utils import get_external_ip, get_sg_id_from_name
+from utils import get_external_ip, get_sg_id_from_name, get_all_sgs
 import boto3
 
 def sg_close(sg_name=None, all_sgs=False):
-      sg_id = get_sg_id_from_name(sg_name)
-      ec2 = boto3.resource('ec2')
+      if all_sgs:
+            sgs = get_all_sgs()
+      else:
+            ec2 = boto3.resource('ec2')
+            sg_id = get_sg_id_from_name(sg_name)
+            sgs = [ec2.SecurityGroup(sg_id)]
+      
       current_ip = get_external_ip()
       current_cidr = current_ip + "/32"
-      security_group = ec2.SecurityGroup(sg_id)
-      security_group.revoke_ingress(
-                  IpProtocol='tcp',
-                  FromPort=22,
-                  ToPort=22,
-                  CidrIp=current_cidr
-                  )
-
-
-if __name__ == '__main__':
-      import sys
-      sg_name = sys.argv[1]
-      sg_close(sg_name=sg_name)
+      for security_group in sgs:
+            print('Closing SSH to this IP on Security Group %s' % security_group.group_name)
+            security_group.revoke_ingress(
+                        IpProtocol='tcp',
+                        FromPort=22,
+                        ToPort=22,
+                        CidrIp=current_cidr
+                        )
