d033d3abf7b5d3826a1b2d8b4d18926e43e033f5
Guilherme Victal <guilhermevictal em gmail ponto com>
2017-02-08 23:24:00 -0200

    Close all open permissions instead of trying to revoke IP

--- sesame/sg_close.py
+++ sesame/sg_close.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from utils import get_external_ip, get_sg_id_from_name, get_all_sgs
+from ipaddress import IPv4Address
+from sg_list import get_open_permissions
 import boto3
 
 def sg_close(sg_name=None, all_sgs=False):
@@ -12,13 +14,11 @@ def sg_close(sg_name=None, all_sgs=False):
             sg_id = get_sg_id_from_name(sg_name)
             sgs = [ec2.SecurityGroup(sg_id)]
       
-      current_ip = get_external_ip()
-      current_cidr = current_ip + "/32"
+      current_ip = IPv4Address(get_external_ip())
       for security_group in sgs:
-            print('Closing SSH to this IP on Security Group %s' % security_group.group_name)
-            security_group.revoke_ingress(
-                        IpProtocol='tcp',
-                        FromPort=22,
-                        ToPort=22,
-                        CidrIp=current_cidr
-                        )
+            permissions = get_open_permissions(security_group, current_ip)
+            if permissions:
+                  print('Closing SSH to this IP on Security Group %s via permissions:' % security_group.group_name)
+                  security_group.revoke_ingress(IpPermissions=permissions)
+                  for permission in permissions:
+                        print(permission)
